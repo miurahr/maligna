62c0ecefed25bcef34f771532cef4334329009b0
Hiroshi Miura <miurahr@linux.com>
2026-05-25 22:31:21 +0900

    chore: rename sonatype credentials to ossrh credentials in build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,16 +7,16 @@ tasks.wrapper {
     gradleVersion = "8.14.2"
 }
 
-val sonatypeUsername: String? by project
-val sonatypePassword: String? by project
+val ossrhUsername: String? by project
+val ossrhPassword: String? by project
 
 nexusPublishing.repositories {
     sonatype {
         nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
         snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
-        if (sonatypeUsername != null && sonatypePassword != null) {
-            username.set(sonatypeUsername)
-            password.set(sonatypePassword)
+        if (ossrhUsername != null && ossrhPassword != null) {
+            username.set(ossrhUsername)
+            password.set(ossrhPassword)
         } else {
             username.set(System.getenv("SONATYPE_USER"))
             password.set(System.getenv("SONATYPE_PASS"))
